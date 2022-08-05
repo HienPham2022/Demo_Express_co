25570d3a110fe00c82b9aef91f0c7ace681cbe46
hienpham <pvhien.ehs.2020@gmail.com>
2022-08-05 18:52:38 +0700

    abc

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send('hello world')
 });
+app.get('/abc',(req,res)=>{
+    res.send('hello from abc');
+});
 app.set('port',process.env.PORT || 3000);
 app.listen(app.get('port'),()=>{
     console.log(`server is listening on port ${app.get('port')} `);
